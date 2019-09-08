8ea4c66dcb461136e06ba1f1bdf49750a4253d44
Amy Young <amy.kristen.young@gmail.com>
2019-09-08 15:38:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,10 +10,6 @@ def turn(board)
 print "Please enter 1-9:"
 input = gets.strip
 input_to_index(input)
-if valid_move?(board, index)
-  move(board,index,playercharacter = X)
-else
-  print "Incorrect value, please enter 1 - 9:"
 end
 
 
